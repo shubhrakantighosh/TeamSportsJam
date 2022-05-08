0155d6134e5f9bf79d4908fdf1fc58e51a93644c
karishma24-max <chauhankarishma824@gmail.com>
2022-05-08 22:43:46 +0530

    changes

--- signin/login.js
+++ signin/login.js
@@ -15,17 +15,25 @@ function sign(){
    
     
    //localStorage.setItem("signuppage2",JSON.stringify(signarr2))
-
-    for(var i=0;i<signarr.length;i++)
-    {
-    
-       
-        if(email2 == signarr[i].email&& passwrd2 ==signarr[i].passwrd)
-        {
+var count=0;
+     for(var i=0;i<signarr.length;i++)
+     {
+         if(count==0)
+    if(email2 == signarr[i].email&& passwrd2 ==signarr[i].passwrd)
+       {
+        
 alert("signup successfully")
-        }
-        else{
-            alert("invalid email or password")
-        }
+count++;
+        
+       
+     }
     }
+    if(count==0)
+    {
+    if(email2 != signarr.email&& passwrd2 !=signarr.passwrd)
+    {
+        alert("invalid email ")
+        count++;
+    }
+}
 }
