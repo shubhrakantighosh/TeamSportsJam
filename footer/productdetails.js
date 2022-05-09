4ff643266f85316e10c7c1271752477f67c7251e
yatinkumar01 <yatins9152015@gmail.com>
2022-05-09 15:29:25 +0530

    ghanta solved

--- footer/productdetails.js
+++ footer/productdetails.js
@@ -1,4 +1,24 @@
 document.querySelector("#btn").addEventListener("click",myfun)
 
 function myfun() {
+ window.location.href="/cart/cart.html";
 
+
+        
+            var cartObj={
+                productimg:"https://storage.sg.content-cdn.io/cdn-cgi/image/width=500,height=500,quality=75,format=auto,fit=cover,g=top/in-resources/b368029c-a4dd-448a-a888-58348cb1b144/Images/ProductImages/Source/YNAKSU5BNL.jpg",
+        
+                productTitle:"Yonex Akayu Super 5 Badminton Shoes (Black/Neon Lime)",
+        
+                productStrick:3890,
+        
+                productPrice:2299,
+                offer:41,
+                rating:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXoT2xTPkYMFeXJ9aVDeqxZQQN9FJ6U1sisCn9Ekd1mVokUw_qnSslJPJVyB5UDKCn4w&usqp=CAU",
+        
+        
+            }
+           
+            localStorage.setItem("cartdata",JSON.stringify(cartObj))
+        
+}
